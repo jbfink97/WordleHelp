29deca634a9b1669cbecb20cf51fcda57bf34eb4
jbfink97 <101232716+jbfink97@users.noreply.github.com>
2022-06-29 20:33:07 -0400

    Update wordlesearch.py
    fixed typo on "expanding"

--- wordlesearch.py
+++ wordlesearch.py
@@ -76,7 +76,7 @@ for i in range(6):
     # if all tiles are green, the game is over and the loop breaks
     if feedback == "ggggg":
         print("nice! you won the big game and you only had to use this stupid program to help")
-        print("maybe try expaning your vocab a little more, idiot")
+        print("maybe try expanding your vocab a little more, idiot")
         break
 
     # cannot make changes to a list as you iterate over it, so make current words list into a tuple
